038a1e2e3b5d30337800cfd0f8cc0ec8cf6cdcd6
Nasar Eddaoui <nasar165@hotmail.com>
2020-09-01 01:35:58 +0700

    move connection to shared

--- src/src/app/shared/shared.module.ts
+++ src/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { TerminalComponent } from './terminal/terminal.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ConnectionComponent } from '../connection/connection.component';
+import { ConnectionComponent } from './connection/connection.component';
 
 @NgModule({
   declarations: [TerminalComponent, SidebarComponent, ConnectionComponent],
