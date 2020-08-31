ba2dafe821757a3d214c3f7e1eaa53f24b77cc07
Nasar Eddaoui <nasar165@hotmail.com>
2020-08-31 23:27:16 +0700

    add angular particles

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Nasar Eddaoui';
+export class AppComponent implements OnInit {
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+
+  ngOnInit() {
+    this.myStyle = {
+      width: '100%',
+      height: '100%',
+    };
+
+    this.myParams = {
+      particles: {
+        number: {
+          value: 80,
+        },
+      },
+      interactivity: {
+        events: {
+          onhover: {
+            enable: false,
+          },
+          onclick: {
+            enable: false,
+          },
+          resize: false,
+        },
+      },
+    };
+  }
 }
--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { ParticlesModule } from 'angular-particle';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, ParticlesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
